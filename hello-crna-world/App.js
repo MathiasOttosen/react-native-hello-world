2e9f7995613fff64a7446c47a3e1bcfb7f07b46c
Mathias Henrik Ottosen <mathias_ottosen@hotmail.com>
2018-03-21 21:47:13 +0100

    hello-crna-world added some views and routing

--- hello-crna-world/App.js
+++ hello-crna-world/App.js
@@ -1,23 +1,129 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Image, Text, View, Button, Alert, StyleSheet} from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+class Blink extends Component {  
+
+  constructor(props){
+    super(props);
+    this.state = {isShowingText: true};
+  }
+  
+
+  // setInterval(() => {
+  //   this.setState(previousState => {
+  //     return { isShowingText: !previousState.isShowingText };
+  //   });
+  // }, 1000); 
+  _onButtonPressed() {
+    // this.setState(previousState => {
+    //   return { isShowingText: !previousState.isShowingText };
+    // });
+  }
+  
+  render(){
+    let display = this.state.isShowingText ? this.props.name : ' ';
+    return ( 
+      <View style= {{alignItems: 'center'}}>
+        <Button 
+        title = 'Press me' 
+        onPress={() => {
+          this.props.navigation.navigate('Details');
+          this._onButtonPressed();
+        }}
+        />
+        <Text>I was here</Text>
+      </View>
+    )
+  }
+}
+
+class Greetings extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <Text style = {styles.bigblue}>Hello {this.props.name}</Text>
+    )
+  }
+}
+
+class HomeScreen extends React.Component{
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <View style={{
+        flex: 1,
+        justifyContent: 'space-between'
+      }}>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+          <View style={{alignItems: 'center', height: 110, backgroundColor: 'powderblue'}}>
+            <Greetings name='Rexxar' />
+          </View>
+            
+          <View style={{alignItems: 'center', height: 110, backgroundColor: 'skyblue'}}>
+          <Button 
+          title = 'Press me' 
+          onPress={() => {
+            this.props.navigation.navigate('Details');
+            
+            //Alert.alert("AARRRGGGHHH");
+            //this._onButtonPressed();
+          }}
+          />
+          </View>
+
+          <View style={{alignItems: 'center', height: 110, backgroundColor: 'steelblue'}}>
+            <Greetings  name='Dave' />
+          </View>
       </View>
     );
   }
 }
 
+class DetailsScreen extends React.Component {
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: '#000000',
+    }
+  }
+  render() {
+    return (
+      <View style= {{borderColor: '#000000', flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#000000'}}>
+        <Image source={require('./assets/smile.jpg')}/>
+        
+      </View>
+    );
+  }
+}
+
+const RootStack = StackNavigator({
+  Home:  {
+    screen: HomeScreen,
+  },
+  Details: {
+    screen: DetailsScreen,
+  },
+},
+{
+  initialRouteName: 'Home',
+}
+); 
+export default class App extends React.Component {
+  render() {
+    return <RootStack/>;
+  }
+}
+
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
   },
 });
